41489af26a82c34e16b2b214d77235986422e4c0
Leonel Elizondo <leonelelizondo@gmail.com>
2023-01-23 22:03:20 -0600

    Se agregan los modulos

---
+++ database/migrations/2023_01_24_035606_create_caja_movimientos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caja_movimientos', function (Blueprint $table) {
+            $table->id();
+            $table->string('movimiento')->nullable();
+            $table->foreignId('caja_id')->nullable()->constrained('cajas');
+            $table->decimal('monto',10,2)->default(0);
+            $table->integer('tipo')->default(1);
+            $table->integer('estado')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caja_movimientos');
+    }
+};
